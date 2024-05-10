e12c65b31968eb1f96c000c17fd1357ba1c51b64
Rotimi Odunsi <rotimigbengaodunsi@gmail.com>
2024-05-10 13:29:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional JavaScript for smooth scrolling between sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
